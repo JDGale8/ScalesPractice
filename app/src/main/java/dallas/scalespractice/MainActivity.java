75f9db01d6cef2ec83912f7aa090d0102ad14efb
Jake <Jake@JakeGale.com>
2016-04-09 12:30:21 +0100

    Can choose between all scales or just natural scales

--- app/src/main/java/dallas/scalespractice/MainActivity.java
+++ app/src/main/java/dallas/scalespractice/MainActivity.java
@@ -1,19 +1,22 @@
 package dallas.scalespractice;
 
-import android.bluetooth.BluetoothClass;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final String KEY_SCALE = "KEY_SCALE";
     private TextView mScaleText;
     private Button mScaleBtn;
     private MajorScalesPractice mMajorScales = new MajorScalesPractice();
-
+    private RadioGroup mScaleTypeRadio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
         mScaleBtn =  (Button) findViewById(R.id.newScaleBtn);
         mScaleText = (TextView) findViewById(R.id.scaleText);
+        mScaleTypeRadio = (RadioGroup) findViewById(R.id.scaleRadioGroup);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // when the button is clicked, it needs to update mScaleText with a new scale
                 String scale = "";
-                //randomly select a scale
-                scale = MajorScalesPractice.getSimpleMScale();
+                //randomly select either a natural or accidental scale depending on the radio button
+                scale = "";
+
+                Log.d(TAG, "int value of radio button is:" +scale);
+
+                int selScaleType = mScaleTypeRadio.getCheckedRadioButtonId();
+
+                int naturalScaleId = 2131492948;
+                int allScaleId = 2131492949;
+
+                if (selScaleType == naturalScaleId) {
+                    scale = MajorScalesPractice.getSimpleMScale();
+                }
+                else if (selScaleType==allScaleId){
+                    scale = MajorScalesPractice.getFullMScale();
+                }
 
                 //update the scale
                 mScaleText.setText(scale);
