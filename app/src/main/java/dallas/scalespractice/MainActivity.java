bc007588230c052e78cfb242ad889ca1262ca755
Jake <Jake@JakeGale.com>
2016-04-09 12:45:19 +0100

    Added radio buttons for minor, major, and both scales

--- app/src/main/java/dallas/scalespractice/MainActivity.java
+++ app/src/main/java/dallas/scalespractice/MainActivity.java
@@ -8,15 +8,20 @@ import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final String KEY_SCALE = "KEY_SCALE";
     private TextView mScaleText;
+    private TextView mMajMinText;
     private Button mScaleBtn;
     private MajorScalesPractice mMajorScales = new MajorScalesPractice();
     private RadioGroup mScaleTypeRadio;
+    private RadioGroup mMajMinRadio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         mScaleBtn =  (Button) findViewById(R.id.newScaleBtn);
         mScaleText = (TextView) findViewById(R.id.scaleText);
+        mMajMinText = (TextView) findViewById(R.id.majorMinorText);
         mScaleTypeRadio = (RadioGroup) findViewById(R.id.scaleRadioGroup);
+        mMajMinRadio = (RadioGroup) findViewById(R.id.majMinRadioGroup);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -35,22 +42,61 @@ public class MainActivity extends AppCompatActivity {
                 //randomly select either a natural or accidental scale depending on the radio button
                 scale = "";
 
-                Log.d(TAG, "int value of radio button is:" +scale);
+                int selMajMin = mMajMinRadio.getCheckedRadioButtonId();
+
+                int majScaleId = 2131492951;
+                int minScaleId = 2131492952;
+                int bothScaleId = 2131492953;
+
+
+                Log.d(TAG, "int value of maj/min radio button is:" +selMajMin);
 
                 int selScaleType = mScaleTypeRadio.getCheckedRadioButtonId();
 
                 int naturalScaleId = 2131492948;
                 int allScaleId = 2131492949;
 
-                if (selScaleType == naturalScaleId) {
-                    scale = MajorScalesPractice.getSimpleMScale();
-                }
-                else if (selScaleType==allScaleId){
-                    scale = MajorScalesPractice.getFullMScale();
-                }
+                //if / else statement depending on if our choice is major or minor
+                if(selMajMin==majScaleId) {
+                    mMajMinText.setText("Major");
+                    if (selScaleType == naturalScaleId) {
+                        scale = MajorScalesPractice.getSimpleMScale();
+                    } else if (selScaleType == allScaleId) {
+                        scale = MajorScalesPractice.getFullMScale();
+                    }
 
-                //update the scale
-                mScaleText.setText(scale);
+                    //update the scale
+                    mScaleText.setText(scale);
+                }
+                else if(selMajMin==minScaleId) {
+                    mMajMinText.setText("Minor");
+                    if (selScaleType == naturalScaleId) {
+                        scale = MajorScalesPractice.getSimpleMScale();
+                    } else if (selScaleType == allScaleId) {
+                        scale = MajorScalesPractice.getFullMScale();
+                    }
+
+                    //update the scale
+                    mScaleText.setText(scale);
+                }
+                else if(selMajMin==bothScaleId) {
+                    Random randomGenerator = new Random();
+                    int randomInt = randomGenerator.nextInt(2);
+                    if (randomInt==0) {
+                        mMajMinText.setText("Major");
+                    }
+                    if (randomInt==1) {
+                        mMajMinText.setText("Minor");
+                    }
+                    if (selScaleType == naturalScaleId) {
+                        scale = MajorScalesPractice.getSimpleMScale();
+                    } else if (selScaleType == allScaleId) {
+                        scale = MajorScalesPractice.getFullMScale();
+                    }
+
+                    //update the scale
+                    mScaleText.setText(scale);
+                }
             }
         };
 
